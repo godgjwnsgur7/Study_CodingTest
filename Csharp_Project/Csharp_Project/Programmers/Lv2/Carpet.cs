0d3694572845a1f000c2d6c85c7e53cebf4ea0df
godgjwnsgur7 <godgjwnsgur7@naver.com>
2023-03-23 20:33:14 +0900

    카펫

---
+++ Csharp_Project/Csharp_Project/Programmers/Lv2/Carpet.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project.Programmers.Lv2
+{
+    internal class Carpet
+    {
+        /* 숫자 변환하기
+        Leo는 카펫을 사러 갔다가 아래 그림(링크)과 같이 중앙에는 노란색으로 칠해져 있고 
+        테두리 1줄은 갈색으로 칠해져 있는 격자 모양 카펫을 봤습니다.
+        https://grepp-programmers.s3.ap-northeast-2.amazonaws.com/files/production/b1ebb809-f333-4df2-bc81-02682900dc2d/carpet.png
+
+        Leo는 집으로 돌아와서 아까 본 카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했지만,
+        전체 카펫의 크기는 기억하지 못했습니다.
+
+        Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 
+        카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+        제한사항
+        갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
+        노란색 격자의 수 yellow는 1 이상 2,000,000 이하인 자연수입니다.
+        카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+        */
+        public static void Main_Carpet(string[] args)
+        {
+            Console.WriteLine($"기댓값 : [4, 3] / 결과 : [{Solution(10, 2)[0]}, {Solution(10, 2)[1]}]");
+            Console.WriteLine($"기댓값 : [3, 3] / 결과 : [{Solution(8, 1)[0]}, {Solution(8, 1)[1]}]");
+            Console.WriteLine($"기댓값 : [8, 6] / 결과 : [{Solution(24, 24)[0]}, {Solution(24, 24)[1]}]");
+        }
+
+        // 풀이시도1.
+        // 노랑 a, 갈색 b, 가로 x, 세로 y 로 놨을 때
+        // 노랑 : a = xy - b, 갈색 : 2x + 2y - 4
+        // 즉, xy = a + b, x + y = (b / 2) + 2
+        // 더한 식의 조건을 대입한 후 곱한 식의 조건에 부합하는지 찾는 방식
+
+        public static int[] Solution(int brown, int yellow)
+        {
+            int[] answer = new int[2]; // 가로, 세로
+
+            int add = (brown / 2) + 2; // 가로 + 세로
+            int xy = brown + yellow; // 가로 * 세로
+
+            int x, y;
+
+            for(int i = 1; i < add - 1; i++)
+            {
+                // 더한 식의 조건을 적용
+                x = i;
+                y = add - x;
+
+                // 곱한 식의 조건을 만족하는 지 확인
+                if (xy == x * y)
+                {
+                    // 최적 값을 찾음
+                    answer[0] = x;
+                    answer[1] = y;
+                    break;
+                }
+            }
+
+            if(answer[0] < answer[1])
+                (answer[0], answer[1]) = (answer[1], answer[0]);
+
+            return answer;
+        }
+    }
+}
---
+++ Csharp_Project/Csharp_Project/Programmers/Lv2/MinimumCommonMultiple.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project.Programmers.Lv2
+{
+    internal class MinimumCommonMultiple
+    {
+        /* 최소 공배수
+        두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다. 
+        예를 들어 2와 7의 최소공배수는 14가 됩니다. 
+        정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다. 
+        n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
+
+        제한 사항
+        arr은 길이 1이상, 15이하인 배열입니다.
+        arr의 원소는 100 이하인 자연수입니다.
+        */
+
+
+        public static void Main_MinimumCommonMultiple(string[] args)
+        {
+            int[] test1 = new int[] { 2, 6, 8, 14 };
+            int[] test2 = new int[] { 1, 2, 3 };
+
+            Console.WriteLine($"기댓값 : 168 / 결과 : {Solution(test1)}");
+            Console.WriteLine($"기댓값 : 6 / 결과 : {Solution(test2)}");
+        }
+
+        // 풀이시도 1.
+        // 인덱스 0번을 기준으로 1번부터 순차적으로 최소공배수를 구해서
+        // 마지막 인덱스까지 최소공배수를 구하면 배열 전체의 최소공배수가 될 것
+
+        public static int Solution(int[] arr)
+        {
+            int answer = arr[0];
+            int answerNum;
+            int arrNum;
+
+            // 0번은 기준 숫자로 answer에 들어가있으므로 1부터 시작
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arrNum = arr[i];
+                answerNum = answer;
+
+                // 현재 숫자와 해당 인덱스의 최소 공배수 구하기 ( 현재 숫자 갱신 )
+                while (answer != arr[i])
+                {
+                    if(answer > arr[i])
+                        arr[i] += arrNum;
+                    else
+                        answer += answerNum;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
