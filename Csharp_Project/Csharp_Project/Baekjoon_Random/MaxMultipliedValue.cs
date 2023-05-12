3d5ccc1c43f420f9589793c1dfaa3cfc7d120805
godgjwnsgur7 <godgjwnsgur7@naver.com>
2023-05-12 16:46:55 +0900

    Add Beakjoon_MultipliedValue
    Add Beakjoon_MultipliedValue

---
+++ Csharp_Project/Csharp_Project/Baekjoon_Random/MaxMultipliedValue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project.Baekjoon_Random
+{
+    /// <summary>
+    /// 최대 곱
+    /// https://www.acmicpc.net/problem/1500
+    /// </summary>
+    internal class MaxMultipliedValue
+    {
+        public static void Main_MaxMultipliedValue(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(Solution(int.Parse(s[0]), int.Parse(s[1])));
+        }
+
+        public static long Solution(int addValue, int count)
+        {
+            long maxMult = 1;
+
+            List<int> list = new List<int>();
+
+            int divide = addValue / count;
+            int theRast = addValue % count;
+            
+            for(int i = 0; i < count; i++)
+                list.Add(divide);
+
+            for (int i = 0; i < theRast; i++)
+                list[i] += 1;
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                maxMult *= list[i];
+            }
+
+            return maxMult;
+        }
+    }
+}
--- Csharp_Project/Csharp_Project/Programmers/Lv1/WalkPark.cs
+++ Csharp_Project/Csharp_Project/Programmers/Lv1/WalkPark.cs
@@ -10,40 +10,10 @@ namespace Csharp_Project.Programmers.Lv1
     {
         /* 공원 산책
         https://school.programmers.co.kr/learn/courses/30/lessons/172928
-        나다니는 길을 'O', 장애물을 'X'로 나타낸 직사각형 격자 모양의 공원에서 로봇 강아지가 산책을 하려합니다.
-        산책은 로봇 강아지에 미리 입력된 명령에 따라 진행하며, 명령은 다음과 같은 형식으로 주어집니다.
-
-        ["방향 거리", "방향 거리" … ]
-        예를 들어 "E 5"는 로봇 강아지가 현재 위치에서 동쪽으로 5칸 이동했다는 의미입니다. 
-        로봇 강아지는 명령을 수행하기 전에 다음 두 가지를 먼저 확인합니다.
-
-        주어진 방향으로 이동할 때 공원을 벗어나는지 확인합니다.
-        주어진 방향으로 이동 중 장애물을 만나는지 확인합니다.
-        위 두 가지중 어느 하나라도 해당된다면, 로봇 강아지는 해당 명령을 무시하고 다음 명령을 수행합니다.
-        공원의 가로 길이가 W, 세로 길이가 H라고 할 때, 공원의 좌측 상단의 좌표는 (0, 0), 
-        우측 하단의 좌표는 (H - 1, W - 1) 입니다.
-
-        공원을 나타내는 문자열 배열 park, 로봇 강아지가 수행할 명령이 담긴 문자열 배열 routes가 매개변수로 주어질 때, 로봇 강아지가 모든 명령을 수행 후 놓인 위치를 [세로 방향 좌표, 가로 방향 좌표] 순으로 배열에 담아 return 하도록 solution 함수를 완성해주세요.
-
-        제한사항
-        3 ≤ park의 길이 ≤ 50
-        3 ≤ park[i]의 길이 ≤ 50
-        park[i]는 다음 문자들로 이루어져 있으며 시작지점은 하나만 주어집니다.
-        S : 시작 지점
-        O : 이동 가능한 통로
-        X : 장애물
-        park는 직사각형 모양입니다.
-        1 ≤ routes의 길이 ≤ 50
-        routes의 각 원소는 로봇 강아지가 수행할 명령어를 나타냅니다.
-        로봇 강아지는 routes의 첫 번째 원소부터 순서대로 명령을 수행합니다.
-        routes의 원소는 "op n"과 같은 구조로 이루어져 있으며, op는 이동할 방향, n은 이동할 칸의 수를 의미합니다.
-        op는 다음 네 가지중 하나로 이루어져 있습니다.
-        N : 북쪽으로 주어진 칸만큼 이동합니다.
-        S : 남쪽으로 주어진 칸만큼 이동합니다.
-        W : 서쪽으로 주어진 칸만큼 이동합니다.
-        E : 동쪽으로 주어진 칸만큼 이동합니다.
-        1 ≤ n ≤ 9
-       */
+        
+        런타임 에러가 부분적으로 뜨는 데 이유를 모르겠음
+        이유는 진짜 없었음 사이트 오류였나봄ㅠㅠ
+        */
 
         public static void Main_WalkPark(string[] args)
         {
@@ -54,19 +24,59 @@ namespace Csharp_Project.Programmers.Lv1
             string[] test3 = new string[] { "OSO", "OOO", "OXO", "OOO" };
             string[] test03 = new string[] { "E 2", "S 3", "W 1" };
 
-            Console.WriteLine($"기댓값 : [4, 3] / 결과 : [{Solution(test1, test01)[0]}, {Solution(test1, test01)[1]}]");
-            Console.WriteLine($"기댓값 : [4, 3] / 결과 : [{Solution(test2, test02)[0]}, {Solution(test2, test02)[1]}]");
-            Console.WriteLine($"기댓값 : [4, 3] / 결과 : [{Solution(test3, test03)[0]}, {Solution(test3, test03)[1]}]");
+            Console.WriteLine($"기댓값 : [2, 1] / 결과 : [{Solution(test1, test01)[0]}, {Solution(test1, test01)[1]}]");
+            Console.WriteLine($"기댓값 : [0, 1] / 결과 : [{Solution(test2, test02)[0]}, {Solution(test2, test02)[1]}]");
+            Console.WriteLine($"기댓값 : [0, 0] / 결과 : [{Solution(test3, test03)[0]}, {Solution(test3, test03)[1]}]");
         }
-
-        // 풀이시도1.
-        // 
 
         public static int[] Solution(string[] park, string[] routes)
         {
             int[] currPosition = new int[2] { -1, -1 };
 
-            // 반성 ㄱ
+            // 출발 위치 찾기
+            for(int i = 0; i < park.Length; i++)
+            {
+                for(int j = 0; j < park[i].Length; j++)
+                {
+                    if(park[i][j] == 'S')
+                    {
+                        currPosition[0] = i;
+                        currPosition[1] = j;
+                        break;
+                    }
+                }
+                if (currPosition[0] != -1 && currPosition[1] != -1)
+                    break;
+            }
+
+            // 길 찾기
+            for (int i = 0; i < routes.Length; i++)
+            {
+                int moveToY, moveToX;
+                moveToY = currPosition[0];
+                moveToX = currPosition[1];
+                bool isMove = true;
+
+                for (int j = 0; j < (int)routes[i][2] - (int)'0'; ++j)
+                {
+                    if (routes[i][0] == 'N') moveToY--;
+                    else if (routes[i][0] == 'S') moveToY++;
+                    else if (routes[i][0] == 'E') moveToX++;
+                    else if (routes[i][0] == 'W') moveToX--;
+                    else Console.WriteLine("범위 벗어남");
+
+                    if (moveToY < 0 || moveToY >= park.Length ||
+                        moveToX < 0 || moveToX >= park[0].Length ||
+                        park[moveToY][moveToX] == 'X')
+                    {
+                        isMove = false;
+                        break;
+                    }
+                }
+
+                if(isMove)
+                    currPosition = new int[2] { moveToY, moveToX };
+            }
             
             return currPosition;
         }
